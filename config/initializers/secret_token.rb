3716a1c167a17782a27e25b9531552868d1bd29d
Roger Dowley <rogerdowley@hotmail.com>
2012-11-25 14:06:23 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ambulance3::Application.config.secret_token = 'b6de460edb8058a4acc4fb1dd0f669ca1e1cad224d22d2386614da0ca087a4e2f6426a961c47f9810b0b801063cedafade7e489b6f1407796be7664b279cab41'
